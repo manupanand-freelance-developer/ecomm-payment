18292cf8db05e054e328587db7d0b7e866ad7ac3
manupanand <manupanandindianinvestor@gmail.com>
2025-06-03 15:10:18 +0530

    updated

--- run.sh
+++ run.sh
@@ -2,33 +2,33 @@
 # to make sure that environment variables are present before running image
 
 
-if [ -z "${CART_HOST}"]; then
+if [ -z "${CART_HOST}" ]; then
     echo Input CART_HOST is missing
     exit 1
 fi
-if [ -z "${CART_PORT}"]; then
+if [ -z "${CART_PORT}" ]; then
     echo Input CART_PORT is missing
     exit 1
 fi
-if [ -z "${USER_HOST}"]; then
+if [ -z "${USER_HOST}" ]; then
     echo Input USER_HOST is missing
     exit 1
 fi
-if [ -z "${USER_PORT}"]; then
+if [ -z "${USER_PORT}" ]; then
     echo Input USER_PORT is missing
     exit 1
 fi
 
 
-if [ -z "${APMQ_HOST}"]; then
+if [ -z "${APMQ_HOST}" ]; then
     echo Input APMQ_HOST is missing
     exit 1
 fi
-if [ -z "${APMQ_USER}"]; then
+if [ -z "${APMQ_USER}" ]; then
     echo Input APMQ_USER is missing
     exit 1
 fi
-if [ -z "${APMQ_PASS}"]; then
+if [ -z "${APMQ_PASS}" ]; then
     echo Input APMQ_PASS is missing
     exit 1
 fi
